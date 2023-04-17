ae3913a416aa56fc2e22a348a9221ab63b95434a
Ihor849 <333marchelo333@gmail.com>
2023-04-18 01:12:45 +0300

    18.04  01.14

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,20 +4,18 @@ import {EventBord} from 'components/eventBord/EventBord'
 
 
 
+
 export const App = () => {
-    console.log(upcomingEvents)
+    // console.log(upcomingEvents)
     return (
     <div>
         <PageTitle text='24th Core orlds Coalition Conference' />
-        <EventBord upcomingEvents='events'/>   
+        <EventBord events={upcomingEvents } />
+          
     </div>
   )
   
 }
 
-// - FaMapMarkerAlt
-// - FaUserAlt
-// - FaCalendarAlt
-// - FaClock
 
 // text="24th Core orlds Coalition Conference"
