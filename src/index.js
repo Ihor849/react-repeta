30728caf47e7c6ebc93f37aa2c679c91304fe968
Ihor849 <333marchelo333@gmail.com>
2023-04-17 10:16:30 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import "./index.css";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import './index.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from 'components/App';
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
 
 root.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
